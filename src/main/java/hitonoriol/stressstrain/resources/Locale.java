7ccd03acf7e322cfcf5a638bcdf28786f07b486b
Hitonoriol <gorgilike@mail.ru>
2022-03-18 15:51:22 +0200

    Read unicode text from files properly

--- src/main/java/hitonoriol/stressstrain/resources/Locale.java
+++ src/main/java/hitonoriol/stressstrain/resources/Locale.java
@@ -2,13 +2,17 @@ package hitonoriol.stressstrain.resources;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.text.StringSubstitutor;
 
+import hitonoriol.stressstrain.Util;
+
 public class Locale {
 	private final static Locale instance = new Locale();
+	private final static Charset UTF8 = Charset.forName("utf8");
 
 	private final static String PREFIX = "{", SUFFIX = "}";
 	private Map<String, String> locMap = new HashMap<>();
@@ -23,13 +27,14 @@ public class Locale {
 
 		instance.locMap.clear();
 		instance.locMap.putAll(Resources.loadMap("/language/" + lang + ".json", String.class, String.class));
+		instance.locMap.forEach((k, v) -> Util.out("%s: %s", k, v));
 	}
 
 	/* Read a localized file from classpath, replacing localization placeholders with values from currently loaded locale */
 	public static String readFile(String file) {
 		String src;
 		try {
-			src = new String(instance.getClass().getResourceAsStream(file).readAllBytes());
+			src = new String(instance.getClass().getResourceAsStream(file).readAllBytes(), UTF8);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
@@ -40,7 +45,8 @@ public class Locale {
 	/* Load a localized FXML file from classpath */
 	public static <T> T loadFXML(String file) {
 		try {
-			return Resources.fxmlLoader().load(new ByteArrayInputStream(readFile(file).getBytes()));
+			return Resources.fxmlLoader()
+					.load(new ByteArrayInputStream(readFile(file).getBytes(UTF8)));
 		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
