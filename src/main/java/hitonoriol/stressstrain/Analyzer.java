68321ee11d1fec12003630b1c11ef3b4f3374394
Hitonoriol <gorgilike@mail.ru>
2022-03-18 12:44:02 +0200

    Set minimum window size

--- src/main/java/hitonoriol/stressstrain/Analyzer.java
+++ src/main/java/hitonoriol/stressstrain/Analyzer.java
@@ -8,6 +8,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Analyzer extends Application {
+	private static Analyzer application;
+
+	public Analyzer() {
+		application = this;
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -15,13 +20,20 @@ public class Analyzer extends Application {
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
 
+		
 		primaryStage.setTitle("Stress Strain State Analyzer");
 		primaryStage.setScene(scene);
-		primaryStage.show();
+        primaryStage.show();
+        primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.setMinHeight(primaryStage.getHeight());
 	}
 
 	public static void main(String args[]) {
 		Prefs.init();
 		launch(args);
+	}
+
+	public static Analyzer app() {
+		return application;
 	}
 }
